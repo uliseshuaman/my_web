6f5a939419ddce2a0a6f48997bcb5e832125f08f
Ulises Huamán <ul.huaman@gmail.com>
2025-11-11 10:47:35 +0100

    Se ha eliminado archivos de la página web estática

--- header.js
+++
@@ -1,6 +0,0 @@
-  const toggle = document.querySelector('.nav-toggle');
-  const navLinks = document.querySelector('.nav-links');
-
-  toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-  });
